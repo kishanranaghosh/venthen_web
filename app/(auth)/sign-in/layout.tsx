b7733ee2a3f38b5c8ca38e5c92d35aa2f8c7f906
kishanghosh090 <kishanghosh090@gmail.com>
2026-09-23 01:07:03 +0530

    add signup and signin pages

---
+++ app/(auth)/sign-in/layout.tsx
@@ -0,0 +1,5 @@
+export default function LoginLayout({ children }: any) {
+  return (
+    <div className="h-screen flex justify-center items-center">{children}</div>
+  );
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "@/components/ui/theme-provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -58,10 +59,20 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: any) {
   return (
     <html
+      suppressHydrationWarning
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full bg-[#080808] text-[#fafafa]">{children}</body>
+      <body className="min-h-full bg-[#080808] text-[#fafafa]">
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
